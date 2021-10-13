6e6941e4cb44c0e8cab42b5514a06501f4c79a49
kamil <kamil@DESKTOP-9R48HSD>
2021-10-13 19:03:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace DadBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task<string> GetApiKey()
+        {
+            return Environment.GetEnvironmentVariable("DiscordKey", EnvironmentVariableTarget.Machine);
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = await GetApiKey(),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+        }
+    }
+}
